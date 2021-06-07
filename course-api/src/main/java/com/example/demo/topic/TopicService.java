55b5c9c5ce56a6ed3d2726fede17ad630c55efe7
Katekar, Suyog <suyog.katekar@here.com>
2021-06-08 00:27:16 +0530

    post request implementation

--- course-api/src/main/java/com/example/demo/topic/TopicService.java
+++ course-api/src/main/java/com/example/demo/topic/TopicService.java
@@ -2,20 +2,24 @@ package com.example.demo.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private List<Topic> allTopics = Arrays.asList(
+    private List<Topic> allTopics = new ArrayList<>( Arrays.asList(
         new Topic("java", "java course", "learn java man"),
         new Topic("nodejs", "node course", "learn node man"),
         new Topic("eth", "ethereum", "learn eth man")
-    );
+    ));
     public List<Topic> getTopicsService(){
         return allTopics;
     }
     public Topic getTopic(String id){
         return allTopics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    }
+    public void addTopic(Topic topic){
+        allTopics.add(topic);
     }
 }
