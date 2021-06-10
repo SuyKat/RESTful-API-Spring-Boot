e60a176c4fbb2a0e15346953d32c4324f169ccf4
Katekar, Suyog <suyog.katekar@here.com>
2021-06-11 00:43:19 +0530

    adding curd operations

---
+++ course-data-api/src/main/java/com/example/coursedata/course/CourseService.java
@@ -0,0 +1,34 @@
+package com.example.coursedata.course;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService {
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public List<Course> getAllCourses(){
+        List<Course> courseList = new ArrayList<>();
+        courseRepository.findAll().forEach(courseList::add);
+        return courseList;
+    }
+    public Optional<Course> getCourse(String id){
+        return courseRepository.findById(id);
+    }
+    public void addCourse(Course course){
+        courseRepository.save(course);
+    }
+
+    public void updateCourse(Course course) {
+        courseRepository.save(course);
+    }
+
+    public void deleteCourse( String id) {
+        courseRepository.deleteById(id);
+    }
+}
