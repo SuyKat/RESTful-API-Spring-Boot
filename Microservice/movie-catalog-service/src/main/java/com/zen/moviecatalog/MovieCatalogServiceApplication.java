5a8c1d14897e4ea01a8003b57c0712135a98b3af
Katekar, Suyog <suyog.katekar@here.com>
2021-06-17 23:08:59 +0530

    call to movie info service

--- Microservice/movie-catalog-service/src/main/java/com/zen/moviecatalog/MovieCatalogServiceApplication.java
+++ Microservice/movie-catalog-service/src/main/java/com/zen/moviecatalog/MovieCatalogServiceApplication.java
@@ -2,10 +2,15 @@ package com.zen.moviecatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
-
+	@Bean
+	public RestTemplate getRestTemplete(){
+		return new RestTemplate();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
--- Microservice/movie-catalog-service/src/main/java/com/zen/moviecatalog/controller/CatalogController.java
+++ Microservice/movie-catalog-service/src/main/java/com/zen/moviecatalog/controller/CatalogController.java
@@ -1,20 +1,36 @@
 package com.zen.moviecatalog.controller;
 
 import com.zen.moviecatalog.model.CatalogItem;
+import com.zen.moviecatalog.model.Movie;
+import com.zen.moviecatalog.model.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class CatalogController {
+    @Autowired
+    RestTemplate restTemplate = new RestTemplate();
     @RequestMapping("/{userId}")
     List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        return Collections.singletonList(
-            new CatalogItem("Matrix", "living in the matrix", 9)
+        List<Rating> ratingList = Arrays.asList(
+            new Rating("2342", 5),
+            new Rating("2343", 7)
         );
+
+
+        return ratingList.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getId(), Movie.class);
+            return new CatalogItem(movie.getName(), "testing", rating.getRating());
+        }).collect(Collectors.toList());
+
     }
 }
